a5fe507066ba012c00645b7b5cd3fbac9c68b169
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2019-11-20 00:27:05 -0300

    consegui acessar a posição de um elemento da multilista, no caso é o primeiro, dentro de um loop pode ser acessado pelo índice

--- ConjuntosDisjuntos/main.c
+++ ConjuntosDisjuntos/main.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "conjunto.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int compara(void *x, void *y);
+void mostra(void *x);
+
 int main(int argc, char *argv[]) {
+	
+	Conjuntos c;
+	inicializa_conjuntos(&c,sizeof(float));
+	float x = 9;
+	int i;
+	cria_conjuntos(&c,&x,compara);
+	
+	/*
+	
+	void var =  c.multi.cabeca[0].info;
+	peesoal, assim estou conseguindo acessar a posiçao do elemento da multilista
+	c->multi->cabeca[0] : 
+	{
+    	info = 0xb81730, 
+    	proximo = 0x0
+	}
+	*/
+	
+	if(x != 2){
+		printf("passei");
+	}
+	
+	
+	
 	return 0;
+}
+
+void mostra(void *x) {
+    printf("%f\n", *(float*) x);
+}
+
+int compara(void *x, void *y) {
+    float *a = x, *b = y;
+    if (*a > *b) return 1;
+    if (*a < *b) return -1;
+    return 0;
 }
